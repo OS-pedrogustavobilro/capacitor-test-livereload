bbb454f659e3c3940b08f05554276a25f7ee1fc9
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-24 12:20:34 +0000

    fix: Add allowed hosts to open link in WebView

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'capacitor-test-livereload',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    allowNavigation: [
+      '*.ngrok-free.dev'
+    ]
+  }
 };
 
 export default config;
